86f529559368f40e35fcab3877d50d0473fc673c
Blak3Nick <blaken2021@gmail.com>
2018-11-03 14:33:04 -0400

    Created new utilities

--- src/main/java/academy/learnprogramming/controller/TodoItemController.java
+++ src/main/java/academy/learnprogramming/controller/TodoItemController.java
@@ -1,6 +1,8 @@
 package academy.learnprogramming.controller;
 
 import academy.learnprogramming.model.TodoData;
+import academy.learnprogramming.util.Mappings;
+import academy.learnprogramming.util.ViewNames;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -13,8 +15,8 @@ public class TodoItemController {
         return new TodoData();
     }
 
-    @GetMapping("items")
+    @GetMapping(Mappings.ITEMS)
     public String items() {
-        return "items";
+        return ViewNames.ITEMS_LIST;
     }
 }
