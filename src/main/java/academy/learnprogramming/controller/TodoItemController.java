f86a0e6d6c48067228069c7675fa1e11c56ed698
Blak3Nick <blaken2021@gmail.com>
2018-11-04 06:35:34 -0500

    new interface todoitemservice

--- src/main/java/academy/learnprogramming/controller/TodoItemController.java
+++ src/main/java/academy/learnprogramming/controller/TodoItemController.java
@@ -1,6 +1,7 @@
 package academy.learnprogramming.controller;
 
 import academy.learnprogramming.model.TodoData;
+import academy.learnprogramming.model.TodoItem;
 import academy.learnprogramming.service.TodoItemService;
 import academy.learnprogramming.util.Mappings;
 import academy.learnprogramming.util.ViewNames;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TodoItemController {
@@ -29,5 +31,9 @@ public class TodoItemController {
     @GetMapping(Mappings.ITEMS)
     public String items() {
         return ViewNames.ITEMS_LIST;
+    }
+    @PostMapping(Mappings.ADD_ITEM)
+    public String processItem(@ModelAttribute TodoItem todoItem) {
+        return "redirect:/" + Mappings.ITEMS;
     }
 }
--- src/main/java/academy/learnprogramming/util/Mappings.java
+++ src/main/java/academy/learnprogramming/util/Mappings.java
@@ -3,7 +3,7 @@ package academy.learnprogramming.util;
 public final class Mappings {
     //constants
     public static final String ITEMS = "items";
-
+    public static final String ADD_ITEM = "addItem";
     private Mappings() {
 
     }
