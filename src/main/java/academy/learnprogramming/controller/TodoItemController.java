f6a0f7bf5fb1c82bb8579845d9504bfded189884
Blak3Nick <blaken2021@gmail.com>
2018-11-05 15:27:26 -0500

    new interface todoitemservice

--- src/main/java/academy/learnprogramming/controller/TodoItemController.java
+++ src/main/java/academy/learnprogramming/controller/TodoItemController.java
@@ -40,20 +40,31 @@ public class TodoItemController {
         return ViewNames.ITEMS_LIST;
     }
     @GetMapping(Mappings.ADD_ITEM)
-    public String addEditItem(Model model){
-        TodoItem todoItem = new TodoItem("", "", LocalDate.now());
+    public String addEditItem(@RequestParam(required = false, defaultValue = "-1") int id, Model model){
+        log.info("editing id {]", id);
+        TodoItem todoItem = todoItemService.getItem(id);
+        if(todoItem == null) {
+             todoItem = new TodoItem("", "", LocalDate.now());
+        }
         model.addAttribute(AttributeNames.TODO_ITEM, todoItem);
         return ViewNames.ADD_ITEM;
     }
     @PostMapping(Mappings.ADD_ITEM)
     public String processItem(@ModelAttribute(AttributeNames.TODO_ITEM) TodoItem todoItem) {
         log.info("todoItem from form = {}", todoItem);
-        todoItemService.addItem(todoItem);
+        if(todoItem.getId() ==0){
+            todoItemService.addItem(todoItem);
+        } else {
+            todoItemService.updateItem(todoItem);
+        }
+
         return "redirect:/" + Mappings.ITEMS;
     }
     @GetMapping(Mappings.DELETE_ITEM)
     public String deleteItem(@RequestParam int id) {
+        log.info("Deleting item with id= {}", id);
         todoItemService.removeItem(id);
         return "redirect:/" + Mappings.ITEMS;
     }
+
 }
